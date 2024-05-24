d30d3f7a31cc0df4cae8bc9c35e542ee90a9b6d9
Quyền Lê <daiquyen0@gmail.com>
2024-05-24 16:40:53 +0700

    test

--- ECommerceMVC/ECommerceMVC/obj/Debug/net8.0/ECommerceMVC.AssemblyInfo.cs
+++ ECommerceMVC/ECommerceMVC/obj/Debug/net8.0/ECommerceMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04af57cf7451a9354be61e2b5829b5f6745cf1c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f810ea5b180b66c96756ac3270147e775265d67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
